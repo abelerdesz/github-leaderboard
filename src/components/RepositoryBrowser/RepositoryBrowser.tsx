cf23744f0bbb415e446b107809c996a4ba334e22
Ábel Erdész <abel.erdesz@gmail.com>
2022-12-08 12:19:48 +0100

    feat(RepositoryBrowser): more accessible tab and select usage

--- src/components/RepositoryBrowser/RepositoryBrowser.tsx
+++ src/components/RepositoryBrowser/RepositoryBrowser.tsx
@@ -1,102 +1,80 @@
-import React, { useEffect, useMemo, useState } from "react";
+import { useEffect, useState } from "react";
 import { RepositoriesResponse, Repository } from "../../models/Repository";
-import {
-  Alert,
-  Box,
-  Checkbox,
-  FormControl,
-  Grid,
-  InputLabel,
-  ListItemText,
-  MenuItem,
-  OutlinedInput,
-  Select,
-  Tab,
-  Tabs,
-  Typography
-} from "@mui/material";
+import { Alert, Box, Grid, Tab, Tabs } from "@mui/material";
 import useLocalStorage from "../../hooks/useLocalStorage";
 import RepositoryList from "../RepositoryList";
 import getTimeZoneAdjustedStartDate from "../../utils/getDateString";
+import TabPanel from "../TabPanel";
+import RepositoryLanguageFilter from "../RepositoryLanguageFilter";
 
 export default function RepositoryBrowser() {
-  const [repos, setRepos] = useState<Repository[]>([]);
+  const SEARCH_TIMESPAN_DAYS = 7;
+
+  const [repositories, setRepos] = useState<Repository[]>([]);
   const [error, setError] = useState(false);
   const [currentTab, setCurrentTab] = useState("all");
-  const [favoriteRepoIds, setFavoriteRepoIds] = useLocalStorage<number[]>(
-    "favorite-repos",
-    []
-  );
-  const [languages, setLanguages] = useState<string[]>([]);
+  const [favoriteRepositoryIds, setFavoriteRepositoryIds] = useLocalStorage<
+    number[]
+  >("favorite-repositories", []);
+  const [allLanguages, setAllLanguages] = useState<string[]>([]);
   const [favoriteLanguages, setFavoriteLanguages] = useLocalStorage<string[]>(
     "favorite-languages",
     []
   );
-  const TIMESPAN_DAYS = 7;
-  const startDateString = useMemo(
-    () => getTimeZoneAdjustedStartDate(new Date(), TIMESPAN_DAYS),
-    []
+  const filteredRepositories = favoriteLanguages.length
+    ? repositories.filter((repo) =>
+        favoriteLanguages.includes(repo.language || "")
+      )
+    : repositories;
+  const favoritedAndFilteredRepositories = filteredRepositories.filter((repo) =>
+    favoriteRepositoryIds.includes(repo.id)
   );
-  const displayedRepos = useMemo(
-    () =>
-      repos.filter(
-        (repo) =>
-          (currentTab === "all" || favoriteRepoIds.includes(repo.id)) &&
-          (favoriteLanguages.length
-            ? favoriteLanguages.includes(repo.language || "")
-            : true)
-      ),
-    [repos, currentTab, favoriteRepoIds, favoriteLanguages]
-  );
+
+  function onAddRepositoryToFavorites(id: number) {
+    const index = favoriteRepositoryIds.indexOf(id);
+    if (index > -1) {
+      setFavoriteRepositoryIds([
+        ...favoriteRepositoryIds.slice(0, index),
+        ...favoriteRepositoryIds.slice(index + 1)
+      ]);
+    } else {
+      setFavoriteRepositoryIds([...favoriteRepositoryIds, id]);
+    }
+  }
 
   useEffect(() => {
-    async function getRepos() {
+    function extractRepositoryLanguages(repositories: Repository[]) {
+      const languages: string[] = [];
+
+      repositories.forEach((repo) => {
+        if (repo.language && !languages.includes(repo.language)) {
+          languages.push(repo.language);
+        }
+      });
+
+      return languages;
+    }
+
+    async function fetchRepositories() {
+      const startDateString = getTimeZoneAdjustedStartDate(
+        new Date(),
+        SEARCH_TIMESPAN_DAYS
+      );
+
       try {
-        const repos: RepositoriesResponse = await fetch(
+        const repositories: RepositoriesResponse = await fetch(
           `https://api.github.com/search/repositories?q=created:%3E${startDateString}&sort=stars&order=desc`
-        ).then((resp) => resp.json());
+        ).then((response) => response.json());
 
-        setRepos(repos.items);
-        setLanguages(() => {
-          const languages: string[] = [];
-
-          repos.items.forEach((repo) => {
-            if (repo.language && !languages.includes(repo.language)) {
-              languages.push(repo.language);
-            }
-          });
-
-          return languages;
-        });
+        setRepos(repositories.items);
+        setAllLanguages(extractRepositoryLanguages(repositories.items));
       } catch {
         setError(true);
       }
     }
 
-    getRepos();
-  }, [startDateString]);
-
-  function onFavorite(id: number) {
-    const favoriteIndex = favoriteRepoIds.indexOf(id);
-    if (favoriteIndex > -1) {
-      setFavoriteRepoIds([
-        ...favoriteRepoIds.slice(0, favoriteIndex),
-        ...favoriteRepoIds.slice(favoriteIndex + 1)
-      ]);
-    } else {
-      setFavoriteRepoIds([...favoriteRepoIds, id]);
-    }
-  }
-
-  function setFavoriteLanguage(name: string | string[]) {
-    if (Array.isArray(name)) {
-      setFavoriteLanguages(name);
-    } else {
-      setFavoriteLanguages([name]);
-    }
-  }
-  const ITEM_HEIGHT = 48;
-  const ITEM_PADDING_TOP = 8;
+    fetchRepositories();
+  }, []);
 
   return (
     <>
@@ -107,62 +85,38 @@ export default function RepositoryBrowser() {
             onChange={(_, value) => setCurrentTab(value)}
           >
             <Tab value="all" label="All repositories" />
-            <Tab value="favoriteRepoIds" label="Favorites" />
+            <Tab value="favorites" label="Favorites" />
           </Tabs>
         </Grid>
         <Grid item xs={5} />
-        <Grid item xs={3}>
-          <FormControl fullWidth>
-            <InputLabel
-              id="language-select-label"
-              size="small"
-              color="secondary"
-            >
-              Language
-            </InputLabel>
-            <Select
-              color="secondary"
-              labelId="language-select-label"
-              id="language-select"
-              multiple
-              value={favoriteLanguages}
-              onChange={(event) => setFavoriteLanguage(event.target.value)}
-              input={<OutlinedInput label="Language" size="small" />}
-              renderValue={(selected) => (
-                <Typography variant="body2" pt={0.5}>
-                  {selected.join(", ")}
-                </Typography>
-              )}
-              MenuProps={{
-                PaperProps: {
-                  style: {
-                    maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP
-                    // width: "100%"
-                  }
-                }
-              }}
-            >
-              {languages.map((language) => (
-                <MenuItem key={language} value={language}>
-                  <Checkbox checked={favoriteLanguages.includes(language)} />
-                  <ListItemText primary={language} />
-                </MenuItem>
-              ))}
-            </Select>
-          </FormControl>
+        <Grid item xs={3} display="flex" alignItems="flex-end">
+          <RepositoryLanguageFilter
+            favoriteLanguages={favoriteLanguages}
+            setFavoriteLanguages={setFavoriteLanguages}
+            allLanguages={allLanguages}
+          />
         </Grid>
       </Grid>
-      <Box mt={3}>
+      <Box mt={4}>
         {error && (
           <Alert severity="error">
             There was an error while fetching repositories.
           </Alert>
         )}
-        <RepositoryList
-          displayedRepos={displayedRepos}
-          favoriteRepoIds={favoriteRepoIds}
-          onFavorite={onFavorite}
-        />
+        <TabPanel currentValue={currentTab} value="all">
+          <RepositoryList
+            displayedRepositories={filteredRepositories}
+            favoriteRepositoryIds={favoriteRepositoryIds}
+            onFavorite={onAddRepositoryToFavorites}
+          />
+        </TabPanel>
+        <TabPanel currentValue={currentTab} value="favorites">
+          <RepositoryList
+            displayedRepositories={favoritedAndFilteredRepositories}
+            favoriteRepositoryIds={favoriteRepositoryIds}
+            onFavorite={onAddRepositoryToFavorites}
+          />
+        </TabPanel>
       </Box>
     </>
   );
---
+++ src/components/RepositoryLanguageFilter/RepositoryLanguageFilter.tsx
@@ -0,0 +1,64 @@
+import {
+  Checkbox,
+  FormControl,
+  InputLabel,
+  ListItemText,
+  MenuItem,
+  OutlinedInput,
+  Select,
+  Typography
+} from "@mui/material";
+
+interface Props {
+  allLanguages: string[];
+  favoriteLanguages: string[];
+  setFavoriteLanguages: (names: string[]) => void;
+}
+
+export default function RepositoryLanguageFilter({
+  allLanguages,
+  favoriteLanguages,
+  setFavoriteLanguages
+}: Props) {
+  function onSelection(name: string | string[]) {
+    if (Array.isArray(name)) {
+      setFavoriteLanguages(name);
+    } else {
+      setFavoriteLanguages([name]);
+    }
+  }
+
+  return (
+    <FormControl fullWidth>
+      <InputLabel id="language-select-label" size="small" color="secondary">
+        Languages
+      </InputLabel>
+      <Select
+        multiple
+        id="language-select"
+        labelId="language-select-label"
+        value={favoriteLanguages}
+        onChange={(event) => onSelection(event.target.value)}
+        input={<OutlinedInput label="Languages" size="small" />}
+        renderValue={(selected) => (
+          <Typography variant="body2" pt={0.5}>
+            {selected.join(", ")}
+          </Typography>
+        )}
+        color="secondary"
+        MenuProps={{
+          PaperProps: {
+            elevation: 12
+          }
+        }}
+      >
+        {allLanguages.map((language) => (
+          <MenuItem key={language} value={language}>
+            <Checkbox checked={favoriteLanguages.includes(language)} />
+            <ListItemText primary={language} />
+          </MenuItem>
+        ))}
+      </Select>
+    </FormControl>
+  );
+}
